d505a4c88ef92233ead038ac0d8c1b89177d3ce3
Nuno Torres <nagltorres@gmail.com>
2019-10-01 01:10:10 +0100

    updated app

--- theme/js/apps/guidedTourVilaSassetti_Elm019.js
+++ theme/js/apps/guidedTourVilaSassetti_Elm019.js
@@ -13433,7 +13433,7 @@ var author$project$OurStory$Narrative$hintEn = '( XP XFB MBCX PC XFB XUVGM ... K
 var author$project$OurStory$Narrative$hintPT = '( YP MVYP BWRZBUYP YP XUGMFP ... WPLB 6 NBXUP ... YBLVG2P YVW QBYUVW ) ';
 var author$project$OurStory$Narrative$encriptedMsg = function (lgId) {
 	var encCoords = ' O8T 9E.898 155I 78.A9E ';
-	return (lgId === 'pt') ? ('Coordinates : ' + (encCoords + author$project$OurStory$Narrative$hintPT)) : ('Coordinates : ' + (encCoords + author$project$OurStory$Narrative$hintEn));
+	return (lgId === 'pt') ? ('KPPUYGOVXBW : ' + (encCoords + author$project$OurStory$Narrative$hintPT)) : ('KPPUYGOVXBW : ' + (encCoords + author$project$OurStory$Narrative$hintEn));
 };
 var author$project$OurStory$Narrative$finalMessage = function (lgId) {
 	var _n0 = A3(
